29ffb0f93887c96b5ac4aea65f5c55d864199682
vivekpatel99 <vivpatel99@gmail.com>
2025-04-29 11:34:43 +0200

    project structure setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vivek Patel",
+    author_email="vivpatel99@gmail.com",
+    packages=find_packages(),
+)
